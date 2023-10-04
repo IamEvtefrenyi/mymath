578045f6455003ace23619a52c2238fd7229523f
IamEvtefrenyi <94805140+IamEvtefrenyi@users.noreply.github.com>
2023-10-04 12:48:56 +0300

    Create mymath.go

---
+++ mymath.go
@@ -0,0 +1,38 @@
+// Пример кода для иллюстрации задачи
+package mymath
+
+import "math"
+
+// Sqrt - функция для вычисления квадратного корня из числа
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+
+}
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+
+}
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+
+}
+func Floor(x float64) float64 {
+	return math.Floor(x)
+
+}
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+
+}
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+
+}
+func Abs(x float64) float64 {
+	return math.Abs(x)
+
+}
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+
+}
